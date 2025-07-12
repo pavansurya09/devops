7fe10038cceef136692775888aa63c5fef5e6c61
Venkata Pavan Surya Kanakam <venkatapavansuryakanakam@Venkatas-MacBook-Air.local>
2025-07-12 13:07:21 -0700

    this is added by venkat

---
+++ calculator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##############################
+
+# Author : Venkat
+
+# date : 12/07/2025
+
+# This script is for git commands
+
+############################
+
+
+echo "vinayaka"
+
